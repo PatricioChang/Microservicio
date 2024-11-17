80dd7bb493326032280236c32940b4098a6cd1a6
Kassi-2 <karen.fabiana226@gmail.com>
2024-11-17 12:05:13 -0300

    se configuró rabbitmq

--- product-service/src/productos/productos.module.ts
+++ product-service/src/productos/productos.module.ts
@@ -2,10 +2,21 @@ import { Module } from '@nestjs/common';
 import { ProductosService } from './productos.service';
 import { ProductosController } from './productos.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
   providers: [ProductosService],
   controllers: [ProductosController],
-  imports: [PrismaModule],
+  imports: [PrismaModule, ClientsModule.register([
+    {
+      name: 'PRODUCTOS_QUEUE',
+      transport: Transport.RMQ,
+      options: {
+        urls: ['amqp://localhost:5672'],
+        queue: 'productos',
+        queueOptions: { durable: false },
+      },
+    },
+  ]),],
 })
 export class ProductosModule {}
--- product-service/src/productos/productos.service.ts
+++ product-service/src/productos/productos.service.ts
@@ -1,15 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { Productos } from '@prisma/client';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Injectable()
 export class ProductosService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService,
+    @Inject('PRODUCTOS_QUEUE') private client: ClientProxy,
+  ) {}
 
   async crear(data: { nombre: string; descripcion: string; precio: number }): Promise<Productos> {
-    return this.prisma.productos.create({
+    const productoCreado = await this.prisma.productos.create({
       data,
     });
+    this.client.emit('producto_agregado', productoCreado); 
+
+    return productoCreado;
   }
 
   async findAll(): Promise<Productos[]> {
